ba4f024bfe327f21bbcda229ca635ac2de313fc1
Mihlali_Jordan <biz@jordanmihlali.dev>
2021-06-20 20:41:10 +0200

    programmatic navigation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { ROUTES } from "../constants";
+import { useHistory } from "react-router-dom";
 
 const HomeScreen = () => {
+  const history = useHistory();
+
+  const navigate = (route) => {
+    history.push(route);
+  };
   return (
     <div>
       <h1>Home Screen</h1>
+      <button onClick={() => navigate(ROUTES.ABOUT.route)}>
+        {ROUTES.ABOUT.name}
+      </button>
     </div>
   );
 };
